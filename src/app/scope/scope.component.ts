854f3c8358dcd0e3d67b2b31858948f20018c4cd
stef1 <stephane.quint@gmail.com>
2019-11-11 20:32:47 +0100

    Add serverside DS

--- src/app/scope/scope.component.ts
+++ src/app/scope/scope.component.ts
@@ -8,20 +8,35 @@ import {HistoService} from '../histo.service';
 })
 export class ScopeComponent implements OnInit {
 
+  private gridApi;
+  private gridColumnApi;
   @Input() scope: string;
+  private defaultColDef;
+  private rowModelType;
+  private cacheBlockSize;
+  private maxBlocksInCache;
 
   columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
     {headerName: 'Price', field: 'price'}
   ];
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  allData = [
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000}
   ];
-  constructor(private histoService: HistoService) { }
+
+  constructor(private histoService: HistoService) {
+    this.defaultColDef = {
+      width: 120,
+      resizable: true
+    };
+    this.rowModelType = 'serverSide';
+    this.cacheBlockSize = 100;
+    this.maxBlocksInCache = 10;
+  }
 
   ngOnInit() {
     this.startHisto();
@@ -29,5 +44,43 @@ export class ScopeComponent implements OnInit {
 
   startHisto() {
     this.histoService.startHisto(this.scope);
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    const server = this.FakeServer([]);
+    const datasource = this.ServerSideDatasource(server);
+    params.api.setServerSideDatasource(datasource);
+  }
+
+  ServerSideDatasource(server) {
+    return {
+      getRows(params) {
+        setTimeout(() => {
+          const response = server.getResponse(params.request);
+          if (response.success) {
+            params.successCallback(response.rows, response.lastRow);
+          } else {
+            params.failCallback();
+          }
+        }, 500);
+      }
+    };
+  }
+
+  FakeServer(allData) {
+    return {
+      getResponse(request) {
+        console.log('asking for rows: ' + request.startRow + ' to ' + request.endRow);
+        const rowsThisPage = allData.slice(request.startRow, request.endRow);
+        const lastRow = allData.length <= request.endRow ? allData.length : -1;
+        return {
+          success: true,
+          rows: rowsThisPage,
+          lastRow
+        };
+      }
+    };
   }
 }
