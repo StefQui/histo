ec6307ba0051fa191fcfb132a7904f514d4459e8
StefQui <stephane.quint@gmail.com>
2019-11-12 01:01:17 +0100

    work 1

--- src/app/histo.service.ts
+++ src/app/histo.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import {interval, of, Subject} from 'rxjs';
-import {fromArray} from 'rxjs/internal/observable/fromArray';
-import {map, switchMap, take} from 'rxjs/operators';
+import { from, interval, of, range, Subject } from 'rxjs';
+import { fromArray } from 'rxjs/internal/observable/fromArray';
+import { map, switchMap, take } from 'rxjs/operators';
+import { mapTo } from 'rxjs/internal/operators';
+import { endWith } from 'rxjs/internal/operators/endWith';
 
 @Injectable({
   providedIn: 'root'
@@ -11,42 +13,54 @@ export class HistoService {
   histos: any;
   histoSubjectMap: any;
 
+  histoDataNextSubject: any;
+  histoDataCompleteSubject: any;
+
   constructor() {
     this.histoSubjectMap = {};
+    this.histoDataNextSubject = new Subject();
+    this.histoDataCompleteSubject = new Subject();
   }
 
   startHisto(scope: string) {
     console.log('start histo ', scope);
     if (!this.histoSubjectMap[scope]) {
       this.histoSubjectMap[scope] = new Subject();
-      this.histoSubjectMap[scope].pipe(switchMap(() => this.buildData(scope))).subscribe((data => {
-        console.log(data);
-      }));
+      this.histoSubjectMap[scope].pipe(
+        switchMap(() => this.buildData(scope).pipe(endWith('theend')))
+      ).subscribe(data => {
+          console.log('next', data);
+          if (data === 'theend') {
+            this.histoDataCompleteSubject.next();
+            return;
+          }
+          this.histoDataNextSubject.next(data);
+        });
     }
     this.histoSubjectMap[scope].next();
-    // this.histos.scope = interval(1000).pipe(
-    //   take(5),
-    //   map(i => {
-    //     return {
-    //     id: i,
-    //     name: scope + i
-    //   };
-    //   })
-    // );
-    // this.histos.scope.subscribe(
-    //   (aa) => console.log('obs ', aa)
-    // );
   }
 
   private buildData(scope: string) {
     return interval(1000).pipe(
-      take(5),
-      map(i => {
-        return {
-          id: i,
-          name: scope + i
-        };
-      })
-    );
+      take(14),
+      map(i => this.getTen(i)),
+      map((arr: number[]) => this.transform(scope, arr)));
+  }
+
+  private transform(scope: string, arr: number[]): any[] {
+    return arr.map(it => {
+      return {
+        id: it,
+        name: scope + it
+      };
+    });
+  }
+
+  private getTen(i: any): number[] {
+    const result = [];
+    range(i * 10, 10).subscribe((aa) => {
+      result.push(aa);
+    });
+    return result;
   }
 }
--- src/app/scope/scope.component.ts
+++ src/app/scope/scope.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {HistoService} from '../histo.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { HistoService } from '../histo.service';
+import { merge, Observable, Subscription } from 'rxjs/index';
+import { map, startWith, tap } from 'rxjs/internal/operators';
+import 'ag-grid-enterprise';
 
 @Component({
   selector: 'app-scope',
@@ -15,18 +18,16 @@ export class ScopeComponent implements OnInit {
   private rowModelType;
   private cacheBlockSize;
   private maxBlocksInCache;
+  private data = [];
+  private dataIsLoaded = false;
 
   columnDefs = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price'}
+    {headerName: 'Id', field: 'id'},
+    {headerName: 'Name', field: 'name'}
   ];
-
-  allData = [
-    {make: 'Toyota', model: 'Celica', price: 35000},
-    {make: 'Ford', model: 'Mondeo', price: 32000},
-    {make: 'Porsche', model: 'Boxter', price: 72000}
-  ];
+  private dataFetcher: Observable<any>;
+  private datasource: { getRows: ((params) => any) };
+  private server: any;
 
   constructor(private histoService: HistoService) {
     this.defaultColDef = {
@@ -34,52 +35,167 @@ export class ScopeComponent implements OnInit {
       resizable: true
     };
     this.rowModelType = 'serverSide';
-    this.cacheBlockSize = 100;
+    this.cacheBlockSize = 40;
     this.maxBlocksInCache = 10;
   }
 
   ngOnInit() {
-    this.startHisto();
+    // this.startHisto();
+    // this.histoService.histoDataNextSubject.asObservable()
+    //   .subscribe((lines) => this.data.push(lines));
+    // this.histoService.histoDataCompleteSubject.asObservable()
+    //   .subscribe((lines) => this.dataIsLoaded = true);
+    this.dataFetcher = merge(
+      this.histoService.histoDataNextSubject.asObservable().pipe(
+        // tap((lines) => this.data.push(lines)),
+        map((lines) => {
+          return {
+            type: 'lines',
+            lines
+          };
+        })
+      ),
+      this.histoService.histoDataCompleteSubject.asObservable().pipe(
+        // tap(() => this.dataIsLoaded = true),
+        map(() => {
+          return {
+            type: 'end'
+          };
+        })
+      )
+    );
+    this.server = this.FakeServer(this.dataFetcher, this.scope);
+    this.datasource = this.ServerSideDatasource(this.server, this.scope);
+
   }
 
   startHisto() {
+    this.data = [];
+    this.dataIsLoaded = false;
+    this.server.reset();
     this.histoService.startHisto(this.scope);
+    this.gridApi.purgeServerSideCache();
   }
 
   onGridReady(params) {
+    console.log('ongread ready');
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-    const server = this.FakeServer([]);
-    const datasource = this.ServerSideDatasource(server);
-    params.api.setServerSideDatasource(datasource);
+    // const datasource = this.ServerSideDatasource(this.FakeServer(this.dataFetcher));
+    params.api.setServerSideDatasource(this.datasource);
   }
 
-  ServerSideDatasource(server) {
+  dataState() {
+    return {
+      dataIsLoaded: this.dataIsLoaded,
+      data: this.data
+    };
+  }
+
+  ServerSideDatasource(fakeServer, scope: string) {
     return {
       getRows(params) {
-        setTimeout(() => {
-          const response = server.getResponse(params.request);
-          if (response.success) {
-            params.successCallback(response.rows, response.lastRow);
-          } else {
-            params.failCallback();
-          }
-        }, 500);
+        return fakeServer.getResponse(params, scope);
+        // setTimeout(() => {
+        //   const response = server.getResponse(params.request);
+        //   if (response.success) {
+        //     params.successCallback(response.rows, response.lastRow);
+        //   } else {
+        //     params.failCallback();
+        //   }
+        // }, 500);
       }
     };
   }
 
-  FakeServer(allData) {
+  FakeServer(dataFetcher: Observable<any>, scope: string) {
+    let loaded = false;
+    let data = [];
+    let isFirst = true;
+    let sub: Subscription;
     return {
-      getResponse(request) {
-        console.log('asking for rows: ' + request.startRow + ' to ' + request.endRow);
-        const rowsThisPage = allData.slice(request.startRow, request.endRow);
-        const lastRow = allData.length <= request.endRow ? allData.length : -1;
-        return {
-          success: true,
-          rows: rowsThisPage,
-          lastRow
-        };
+      reset() {
+        loaded = false;
+        data = [];
+        isFirst = true;
+      },
+      getResponse(params) {
+        console.log('------------------asking for rows: ' + params.request.startRow + ' to ' + params.request.endRow + ' loaded=' +
+          loaded + ' data.length' + data.length + ' scope=' + scope);
+
+        if (!sub) {
+          params.successCallback([], 0);
+        }
+        if (params.request.endRow < data.length) {
+          const res = data.slice(params.request.startRow, params.request.endRow);
+          params.successCallback(res, loaded ? data.length : -1);
+          return;
+        }
+        if (loaded) {
+          if (params.request.startRow < data.length) {
+            const res = data.slice(params.request.startRow, data.length);
+            params.successCallback(res, data.length);
+          } else {
+            params.successCallback([], data.length);
+          }
+          return;
+        }
+        if (sub) {
+          sub.unsubscribe();
+        }
+        let alreadyFetched = false;
+        sub = dataFetcher.pipe(startWith({
+          type: 'starts'
+        })).subscribe((res) => {
+          console.log('datafetcher', res, params.request, alreadyFetched);
+          if (res.type === 'starts') {
+            if (isFirst) {
+              isFirst = false;
+              params.successCallback([], 0);
+              return;
+            } else {
+              return;
+            }
+          }
+          if (res.type === 'end') {
+              loaded = true;
+          } else {
+            data.push(...res.lines);
+            console.log('push lines ', data.length, res.lines);
+          }
+          if (params.request.endRow < data.length && ! alreadyFetched) {
+            const res1 = data.slice(params.request.startRow, params.request.endRow);
+            console.log('**************** loaded = ', loaded);
+            params.successCallback(res1, data.length);
+            alreadyFetched = true;
+            return;
+          }
+          if (loaded) {
+            if (params.request.startRow < data.length) {
+              const res1 = data.slice(params.request.startRow, data.length);
+              params.successCallback(res1, data.length);
+            } else {
+              params.successCallback([], data.length);
+            }
+            return;
+          }
+        });
+        // this.fetcher.subscribe(() => {
+        //   if (request.endRow < this.data.length) {
+        //     const res = this.data.slice(request.startRow, request.endRow);
+        //     request.successCallback(res, dataState().dataIsLoaded ? dataState().data.length : -1);
+        //     return;
+        //   }
+        // });
+
+
+        //   const rowsThisPage = allData.slice(request.startRow, request.endRow);
+        //   const lastRow = allData.length <= request.endRow ? allData.length : -1;
+        //   return {
+        //     success: true,
+        //     rows: rowsThisPage,
+        //     lastRow
+        //   };
       }
     };
   }
